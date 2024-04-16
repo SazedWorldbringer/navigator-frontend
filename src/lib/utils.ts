0a596e4ac5cc76e1d675f3094a162a6301ccae8a
Atharva Pardeshi <atharva.exe@gmail.com>
2024-04-16 07:40:39 +0530

    chore: setup data conversion for d3

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { WeightedAdjacencyList } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function convertDataForD3(rawData: WeightedAdjacencyList) {
+  const vertices: any[] = [];
+  const links: any[] = [];
+
+  rawData.forEach((nodeData, idx) => {
+    vertices.push({ id: idx, idx })
+
+    nodeData.forEach(edge => {
+      links.push({
+        source: vertices[idx],
+        target: vertices[edge.To],
+        weight: edge.Weight
+      })
+    })
+  })
+  return { vertices, links }
 }
