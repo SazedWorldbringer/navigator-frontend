661141998b08b4a767295f9e92fcd06dcfb842f3
Atharva Pardeshi <atharva.exe@gmail.com>
2024-04-16 07:41:09 +0530

    feat: navbar component

---
+++ src/components/nav.tsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <div className="flex h-16 items-center justify-center py6">
+      <nav>
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Lodestar
+        </h3>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
